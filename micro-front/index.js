12c51a5297edb3e2cc4f37628d66e1b46cb994a1
Diego Pereira <33781733+dhiegopereira@users.noreply.github.com>
2024-12-17 20:11:28 -0300

    Update index.js

--- micro-front/index.js
+++ micro-front/index.js
@@ -7,16 +7,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Configuração do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Função para gerar dinamicamente os apps a partir das variáveis de ambiente
 function getAppsFromEnv() {
   const apps = [];
   const envKeys = Object.keys(process.env);
 
-  // Itera sobre as variáveis de ambiente e busca URLs e Labels
   for (let i = 1; i <= 10; i++) {
     const labelKey = `LABEL_APP${i}`;
     const urlKey = `URL_APP${i}`;
@@ -30,13 +27,11 @@ function getAppsFromEnv() {
   return apps;
 }
 
-// Rota principal
 app.get("/", (req, res) => {
   const apps = getAppsFromEnv();
   res.render("index", { apps });
 });
 
-// Inicializa o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
